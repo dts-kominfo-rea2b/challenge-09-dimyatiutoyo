c743724d5499ceb4e64f74e7df28b56948b1d5a1
Dimyati Utoyo <dimyatiutoyo.b@gmail.com>
2022-06-17 23:30:03 +0700

    modify the code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import './Contact.css';
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <li key={data.name} className='contact-item'>
+            <div className='contact-container'>
+                <img className='image-container' src={data.photo} alt={data.name} />
+                <div className='contact-details'>
+                    <h3 className='contact-name'>{data.name}</h3>
+                    <p className='contact-phone'>{data.phone}</p>
+                    <p className='contact-email'>{data.email}</p>
+                </div>
+            </div>
+            <div className="call-button">
+                <button className='btn btn-link'>CALL</button>
+            </div>
+        </li>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css';
 import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div className='appbar'>
+            <h1>Call a friend</h1>
+            <h5>your friendly contact app</h5>
+        </div>
     )
 }
 
